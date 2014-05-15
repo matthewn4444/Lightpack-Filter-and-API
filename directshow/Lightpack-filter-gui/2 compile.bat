97e4a468daeac09ba6813c1e804108eafae73af7
Matthew Ng <matthewn4444@gmail.com>
2014-05-15 02:19:46 -0400

    Updated the compile bat to include lightpack.node

--- directshow/Lightpack-filter-gui/2 compile.bat
+++ directshow/Lightpack-filter-gui/2 compile.bat
@@ -1,4 +1,5 @@
 rem Compiles and places the nw file into the release and debug folders
 del ..\..\Release\app.nw
-zip ..\..\Release\app.nw index.html package.json *.js
+copy ..\..\Release\lightpack.node lightpack.node
+zip ..\..\Release\app.nw index.html package.json *.js lightpack.node
 copy "..\..\Release\app.nw" "..\..\Debug\app.nw"
