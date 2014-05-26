99039e5294612abebad9b64b2894fdc67b706ee1
Matthew Ng <matthewn4444@gmail.com>
2014-05-26 17:45:42 -0400

    Did not copy the lightpack.node file to correct folder

--- directshow/Lightpack-filter-gui/2 compile.bat
+++ directshow/Lightpack-filter-gui/2 compile.bat
@@ -1,5 +1,5 @@
 rem Compiles and places the nw file into the release and debug folders
 del ..\..\Release\app.nw
-copy ..\..\Release\lightpack.node lightpack\lightpack.node
+copy ..\..\Release\lightpack.node node_modules\lightpack\lightpack.node
 zip -r ..\..\Release\app.nw package.json node_modules\* src\*
 copy "..\..\Release\app.nw" "..\..\Debug\app.nw"
