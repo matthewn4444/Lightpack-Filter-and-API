b695092b49ba20987b0f5ccfc1f3ad9774d996bd
Matthew Ng <matthewn4444@gmail.com>
2014-06-22 22:46:20 -0400

    Moved the node webkit binaries to the nodejs folder

--- directshow/Lightpack-filter-gui/2 compile.bat
+++ directshow/Lightpack-filter-gui/2 compile.bat
@@ -1,5 +1,6 @@
 rem File paths
 set SETUP=setup-files
+set NW_BIN=..\..\nodejs\nw-binaries
 set RELEASE=..\..\Release
 set DEBUG=..\..\Debug
 
@@ -7,18 +8,19 @@ rem Compiles and places the nw file into the release and debug folders
 del %RELEASE%\nw.exe
 del %RELEASE%\setup.iss
 copy %RELEASE%\lightpack.node node_modules\lightpack\lightpack.node
+copy %RELEASE%\app-mutex.node node_modules\lightpack\app-mutex.node
 zip -r app.nw package.json node_modules\* src\*
 
 rem Create the single executable and put it in release & debug
-copy /b %SETUP%\nw\nw.exe+app.nw %RELEASE%\nw.exe
+copy /b %NW_BIN%\nw.exe+app.nw %RELEASE%\nw.exe
 copy "%RELEASE%\nw.exe" "%DEBUG%\nw.exe"
 
 rem Copy the nw.pak and icudt.dll file to those folders
 copy src\images\icon.ico %RELEASE%\icon.ico
-copy %SETUP%\nw\nw.pak %RELEASE%\nw.pak
-copy %SETUP%\nw\nw.pak %DEBUG%\nw.pak
-copy %SETUP%\nw\icudt.dll %RELEASE%\icudt.dll
-copy %SETUP%\nw\icudt.dll %DEBUG%\icudt.dll
+copy %NW_BIN%\nw.pak %RELEASE%\nw.pak
+copy %NW_BIN%\nw.pak %DEBUG%\nw.pak
+copy %NW_BIN%\icudt.dll %RELEASE%\icudt.dll
+copy %NW_BIN%\icudt.dll %DEBUG%\icudt.dll
 copy %SETUP%\setup.iss %RELEASE%\setup.iss
 
 rem Move the bat files (for registering/unregistering the filter)
