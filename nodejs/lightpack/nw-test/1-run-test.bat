064d02a85127960f210490671e6f99a2dd1d6706
Matthew Ng <matthewn4444@gmail.com>
2014-06-22 22:57:13 -0400

    Updated the code for testing lightpack.node

--- nodejs/lightpack/nw-test/1-run-test.bat
+++ nodejs/lightpack/nw-test/1-run-test.bat
@@ -1,8 +1,8 @@
 @echo off
 
 rem Compiles and places the nw file into the release and debug folders
-del ..\..\Release\nw-test.nw
+del nw-test.nw
 copy ..\..\Release\lightpack.node lightpack.node
-zip ..\..\Release\nw-test.nw index.html package.json *.js lightpack.node
+zip nw-test.nw index.html package.json *.js lightpack.node
 
-start ../../Release/nw.exe ../../Release/nw-test.nw
+start ../../nw-binaries/nw.exe nw-test.nw
