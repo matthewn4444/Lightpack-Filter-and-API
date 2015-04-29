a846a6f21bd2d4ec6241834f89ff68e93b6fe37f
Matthew Ng <matthewn4444@gmail.com>
2015-04-29 00:01:35 -0700

    Build all with also build the setup.exe file if Inno Setup is installed

--- directshow/Lightpack-filter-gui/build_all.bat
+++ directshow/Lightpack-filter-gui/build_all.bat
@@ -14,4 +14,21 @@ copy setup-files\bats\post-unpack.bat %RELEASE%\post-unpack.bat
 cd %RELEASE% && %SETUP%\tools\zip -r lightpack-filter icon.ico icudt.dll lightpack.ax nw.exe nw.pak installFilter.bat uninstallFilter.bat post-unpack.bat LICENSE.txt
 del post-unpack.bat
 
+rem Create setup file
+set "LOCATION_X64=C:\Program Files\Inno Setup 5\iscc.exe"
+set "LOCATION_X86=C:\Program Files (x86)\Inno Setup 5\iscc.exe"
+
+if exist "%LOCATION_X64%" (
+    set "COMPILER=%LOCATION_X64%"
+) else if exist "%LOCATION_X86%" (
+    set "COMPILER=%LOCATION_X86%"
+) else (
+    echo Cannot find Inno Setup compiler, did you install it?
+    goto done
+)
+
+echo Using inno setup compiler from %COMPILER%
+"%COMPILER%" "setup.iss"
+:done
+
 cd %OLD_DIR%
